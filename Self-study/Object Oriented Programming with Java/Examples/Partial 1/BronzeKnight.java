b0deaed2231f918ebddcf873af8dd5d2287bb539
Lars17S <efren.garibaldi@gmail.com>
2019-02-11 22:32:23 -0700

    d

--- Self-study/Object Oriented Programming with Java/Examples/Partial 1/BronzeKnight.java
+++ Self-study/Object Oriented Programming with Java/Examples/Partial 1/BronzeKnight.java
@@ -1,4 +1,9 @@
-public class BronzeKnight {
+public class BronzeKnight extends Character{
   private Hability[] habilities;
   private Object[] inventory;
+
+  public BronzeKnight(String name, int health, int level, int energy, int attack, int defense) {
+    super(name, health, level, energy, attack, defense);
+  }
+
 }
