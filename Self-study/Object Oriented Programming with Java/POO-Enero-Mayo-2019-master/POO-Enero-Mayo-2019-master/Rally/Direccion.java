7316c44d048e9e439318e658d56c649f00a47dc9
Lars17S <efren.garibaldi@gmail.com>
2019-02-22 10:37:21 -0600

    f

---
+++ Self-study/Object Oriented Programming with Java/POO-Enero-Mayo-2019-master/POO-Enero-Mayo-2019-master/Rally/Direccion.java
@@ -0,0 +1,9 @@
+public class Direccion {
+  private String direccion;
+
+
+  public Direccion (String direccion) {
+    this.direccion = direccion;
+  }
+
+}
---
+++ Self-study/Object Oriented Programming with Java/POO-Enero-Mayo-2019-master/POO-Enero-Mayo-2019-master/Rally/Ganador.java
@@ -0,0 +1,8 @@
+public class Ganador extends Usuario{
+
+
+  public String avanzar(String ganas, int cuanto) {
+		System.out.println("Hola soy Efren y Andrea, y gané.");
+    return ganas;
+	}
+}
